e3c2761e9c06dcc087d52c249ddd99b830f6b85c
sarisia <sarisiaice@gmail.com>
2020-09-09 04:02:36 +0900

    test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ console.log(nowstr('en-US'))
 console.log(nowstr('ja-JP'))
 
 function nowstr(locale) {
-    return (new Date()).toLocaleString(locale)
+    return (new Date()).toLocaleString(locale, { month: 'long', weekday: 'long' })
 }
