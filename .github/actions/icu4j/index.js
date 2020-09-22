b2c52d31970e8d4393360d04edddfbfe28b60438
Sarisia <sarisiaice@gmail.com>
2020-09-22 15:26:56 +0000

    icu4j

---
+++ .github/actions/icu4j/index.js
@@ -0,0 +1,5 @@
+console.log(process.config.variables)
+console.log(process.versions)
+
+console.log((new Date()).toLocaleString('en-US', { month: 'long', weekday: 'long' }))
+console.log((new Date()).toLocaleString('ja-JP', { month: 'long', weekday: 'long' }))
